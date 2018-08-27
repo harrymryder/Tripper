8895621ee7fac311b6126348d9ff8a69927d5cc3
Harry Ryder <harrymryder@gmail.com>
2018-08-27 15:46:27 +0100

    Generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [:show, :edit, :update]
+
+  resources :trips do
+    resources :legs
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
