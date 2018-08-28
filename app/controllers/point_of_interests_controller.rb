4382cd44d0cf1ce7041eef7ef3c96b38f0e2246e
George Delori <gdelori@yahoo.com>
2018-08-28 16:13:08 +0100

    scraper seed 1"

---
+++ app/controllers/point_of_interests_controller.rb
@@ -0,0 +1,16 @@
+class PointOfInterestsController < ApplicationController
+
+  def new
+    @poi = PointOfInterest.new
+  end
+
+  def create
+    @poi = PointOfInterest.new(poi_params)
+  end
+
+  private
+
+  def poi_params
+    params.require(:point_of_interest).permit(:name, :lat, :long, :description, :photo, :url, :type)
+  end
+end
