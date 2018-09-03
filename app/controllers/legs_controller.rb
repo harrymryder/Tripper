11c78470949176d247340f2d23122ea950dbc323
George Delori <gdelori@yahoo.com>
2018-09-03 14:31:02 +0100

    legs solved

--- app/controllers/legs_controller.rb
+++ app/controllers/legs_controller.rb
@@ -3,7 +3,7 @@ class LegsController < ApplicationController
   def create
     @trip = Trip.find(params[:trip_id])
     @poi = PointOfInterest.find(params[:poi_id])
-    @leg = Leg.new(trip: @trip, point_of_interest: @poi)
+    @leg = Leg.new(trip_id: @trip.id, point_of_interest: @poi)
 
     if @leg.save
       redirect_to trip_path(@trip)
@@ -18,15 +18,5 @@ class LegsController < ApplicationController
     @leg = Leg.find(params[:id])
     @leg.destroy
     redirect_to trip_path(@trip)
-  end
-
-  def leg_list
-    @legs = Leg.all
-    @leg_list = []
-    @legs.each do |leg|
-      geocode = [ leg.point_of_interest.lat, leg.point_of_interest.long ]
-      @leg_list << geocode
-    end
-    @leg_list
   end
 end
