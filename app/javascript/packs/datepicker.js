05eb19b14969ad6ea9ff0a278ff3188eafb656da
Chitty Liu <d2921118@gmail.com>
2018-09-07 11:17:48 +0100

    datepicker format done, adjusted the date input box, date in dashboard is in the right format

--- app/javascript/packs/datepicker.js
+++ app/javascript/packs/datepicker.js
@@ -4,7 +4,7 @@ const dateElements = document.querySelectorAll('.flatpickr-input')
 
 dateElements.forEach(dateElement => {
   flatpickr(dateElement, {
-    dateFormat: "Y-m-d",
+    dateFormat: "d-M-Y",
   })
 })
 
